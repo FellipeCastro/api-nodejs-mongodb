ed3e373d6bc444711127aca98ba292c68a5bc912
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-30 16:45:49 -0300

    Conexão com banco de dados MongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+app.use(express.json())
 const PORT = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+const Film = mongoose.model('Film', {
+    title: String,
+    description: String,
+    img: String,
+    trailer: String
+
+})
+
+app.get('/', async (req, res) => {
+    const films = await Film.find()
+    res.send(films)
+})
+
+app.post('/', async (req, res) => {
+    const film = new Film({
+        title: req.body.title,
+        description: req.body.description,
+        img: req.body.img,
+        trailer: req.body.trailer,
+    })
+
+    await film.save()
+    res.send(film)
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em: htps://localhost:${PORT}`)
+    mongoose.connect('mongodb+srv://fehcastru:yB6qa7rcs0C0RzCh@starwars-api.rjbrt7c.mongodb.net/?retryWrites=true&w=majority')
+    console.log(`Servidor rodando em: http://localhost:${PORT}`)
 })
