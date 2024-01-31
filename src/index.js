9d90a352afec66645f8f1925c3dd33c5152899c3
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-31 09:44:50 -0300

    Projeto finalizado

--- src/index.js
+++ src/index.js
@@ -18,6 +18,22 @@ app.get('/', async (req, res) => {
     res.send(films)
 })
 
+app.delete('/:id', async (req, res) => {
+    const film = await Film.findByIdAndDelete(req.params.id)
+    res.send(film)
+})
+
+app.put('/:id', async (req, res) => {
+    const film = await Film.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        description: req.body.description,
+        img: req.body.img,
+        trailer: req.body.trailer,
+    }, {new: true})
+
+    return res.send(film)
+})
+
 app.post('/', async (req, res) => {
     const film = new Film({
         title: req.body.title,
